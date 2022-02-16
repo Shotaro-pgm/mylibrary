365d6e7788b23c9de10f942e50611280edac3fd4
Shotaro Ito <shotaro.ito.appleapple@gmail.com>
2022-02-16 14:34:04 +0900

    homework

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function(){
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('http://XXXXXX.jp/XXX', 'AAAController@bbb');
+  Route::get('admin/profile/create', 'Admin\ProfileController@add');
+  Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+});
