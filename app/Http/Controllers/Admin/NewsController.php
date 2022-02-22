ef7bb07ba01eb8eb5c1d045b5eb18cdc253de253
Shotaro Ito <shotaro.ito.appleapple@gmail.com>
2022-02-22 23:24:28 +0900

    PHP/Laravel 17

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -16,12 +17,13 @@ class NewsController extends Controller
 
     public function create(Request $request)
     {
+
       $this->validate($request, News::$rules);
 
       $news = new News;
       $form = $request->all();
 
-      if (isset($form['image'])) {
+      if ($form['image']) {
         $path = $request->file('image')->store('public/image');
         $news->image_path = basename($path);
       } else {
@@ -30,11 +32,58 @@ class NewsController extends Controller
 
       unset($form['_token']);
       unset($form['image']);
-
       $news->fill($form);
       $news->save();
 
       return redirect('admin/news/create');
     }
-    
+
+    public function index(Request $request)
+    {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+        $posts = News::where('title', $cond_title)->get();
+      } else {
+        $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+
+    public function edit(Request $request)
+    {
+      $news = News::find($request->id);
+      if (empty($news)) {
+        abort(404);
+      }
+      return view('admin.news.edit', ['news_form' => $news]);
+    }
+
+    public function update(Request $request)
+    {
+      $this->validate($request, News::$rules);
+      $news = News::find($requews->id);
+      $news_form = $request->all();
+      if ($request->remove == "true") {
+        $news_form['image_path'] = null;
+      } elseif ($request->file('image')) {
+        $path = $request->file('image')->store('public/image');
+        $news_form['image_path'] = basename($path);
+      } else {
+        $news_form['image_path'] = $news->image_path;
+      }
+
+      unset($news_form['image']);
+      unset($news_form['remove']);
+      unset($news_form['_token']);
+      $news->fill($news_form)->save();
+      return redirect('admin/news');
+    }
+
+    public function delete(Request $request)
+    {
+      $news = News::find($request->id);
+      $news->delete();
+      return redirect('admin/news/');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,30 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
   Route::get('news/create', 'App\Http\Controllers\Admin\NewsController@add')->middleware('auth');
-  Route::post('news/create', 'Admin\NewsController@create');
+  Route::post('news/create', 'App\Http\Controllers\Admin\NewsController@create');
   Route::get('http://XXXXXX.jp/XXX', 'AAAController@bbb')->middleware('auth');
-  Route::get('admin/profile/create', 'Admin\ProfileController@add')->middleware('auth');
-  Route::get('admin/profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-  Route::post('admin/profile/create', 'Admin\ProfileController@create');
-  Route::post('admin/profile/edit', 'Admin\ProfileController@update');
+  Route::get('admin/profile/create', 'App\Http\Controllers\Admin\ProfileController@add')->middleware('auth');
+  Route::get('admin/profile/edit', 'App\Http\Controllers\Admin\ProfileController@edit')->middleware('auth');
+  Route::post('admin/profile/create', 'App\Http\Controllers\Admin\ProfileController@create');
+  Route::post('admin/profile/edit', 'App\Http\Controllers\Admin\ProfileController@update');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+  Route::get('news/create', 'App\Http\Controllers\Admin\NewsController@add')->middleware('auth');
+  Route::post('news/create', 'App\Http\Controllers\Admin\NewsController@create')->middleware('auth');
+  Route::get('news', 'App\Http\Controllers\Admin\NewsController@index')->middleware('auth');
+  Route::get('news/edit', 'App\Http\Controllers\Admin\NewsController@edit')->middleware('auth');
+  Route::post('news/edit', 'App\Http\Controllers\Admin\NewsController@update')->middleware('auth');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+  Route::get('news/create', 'App\Http\Controllers\Admin\NewsController@add')->middleware('auth');
+  Route::post('news/create', 'App\Http\Controllers\Admin\NewsController@create')->middleware('auth');
+  Route::get('news','App\Http\Controllers\Admin\NewsController@index')->middleware('auth');
+  Route::get('news/edit', 'App\Http\Controllers\Admin\NewsController@edit')->middleware('auth');
+  Route::get('news/edit', 'App\Http\Controllers\Admin\NewsController@update')->middleware('auth');
+  Route::get('news/delete', 'App\Http\Controllers\Admin\NewsController@delete')->middleware('auth');
+})
 
 Auth::routes();
 
